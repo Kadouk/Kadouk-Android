429259c9ed1182707f841cccbf8e6d79ba1f4f85
mohamad_zoli <zolfaghari.mh94@gmail.com>
2018-11-21 01:00:55 +0330

    design basic getNumberActivity

--- app/src/main/java/com/kadouk/app/SignUpEnterCodeActivity.java
+++ app/src/main/java/com/kadouk/app/SignUpEnterCodeActivity.java
@@ -4,6 +4,8 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
@@ -33,6 +35,15 @@ public class SignUpEnterCodeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_signup_enter_code);
 
         EditTextCode = findViewById(R.id.signup_edt_code);
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Here's a Snackbar", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+                Log.i("FABB",":)");
+            }
+        });
     }
 
     public void sendCode(View view) {
