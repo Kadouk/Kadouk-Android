0ed2fd6c419a6651559aee99a3c5f78e707c73fc
mohamad_zoli <zolfaghari.mh94@gmail.com>
2018-11-23 00:58:53 +0330

    complete SignUpEnterCodeActivity without upButton

--- app/src/main/java/com/kadouk/app/SignUpEnterCodeActivity.java
+++ app/src/main/java/com/kadouk/app/SignUpEnterCodeActivity.java
@@ -10,32 +10,45 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import com.kadouk.app.model.Response;
 import com.kadouk.app.webService.APIClient;
 import com.kadouk.app.webService.APIInterface;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 import retrofit2.Call;
 import retrofit2.Callback;
+
+import static android.graphics.Color.BLACK;
 
 /**
  * Created by zoli on 08/28/2018.
  */
 
 public class SignUpEnterCodeActivity extends AppCompatActivity {
+
     EditText EditTextCode;
     public static final String MyShPref = "MyPrefers", FirstRun = "run",
             authenticationToken = "Token";
     SharedPreferences SharedPreferences;
     Intent intent;
 
+    Timer timer;
+    TimerTask TimerTask;
+    int  time = 1000, timeLeft = 0;
+    TextView textViewResendCode;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup_enter_code);
-
+        textViewResendCode = (TextView) findViewById(R.id.signup_txv_receive_code);
         EditTextCode = findViewById(R.id.signup_edt_code);
         FloatingActionButton fab = findViewById(R.id.fab_code);
+        startTimer(time);
 //        fab.setOnClickListener(new View.OnClickListener() {
 //            @Override
 //            public void onClick(View view) {
@@ -44,7 +57,21 @@ public class SignUpEnterCodeActivity extends AppCompatActivity {
 //                Log.i("FABB",":)");
 //            }
 //        });
+
+
     }
+
+    View.OnClickListener onclicklistener = new View.OnClickListener() {
+
+        @Override
+        public void onClick(View v) {
+
+            startTimer(time);
+            textViewResendCode.setTextColor(BLACK);
+            textViewResendCode.setOnClickListener(null);
+
+        }
+    };
 
     public void sendCode(View view) {
 
@@ -112,5 +139,46 @@ public class SignUpEnterCodeActivity extends AppCompatActivity {
         Intent intent = new Intent(SignUpEnterCodeActivity.this, SignUpActivity.class);
         finish();
         startActivity(intent);
+    }
+
+    private class timerTask extends TimerTask {
+
+        int timeCounter = 0, lastPeriod = 10;
+
+        @Override
+        public void run() {
+
+            timeCounter ++;
+
+            runOnUiThread (new Runnable() {
+
+                @Override
+                public void run() {
+
+                    timeLeft = lastPeriod - timeCounter;
+                    textViewResendCode.setText( getString(R.string.wait_to_receive_code) + "  " +String.format("%02d",00) + ":"+ String.format("%02d",timeLeft));
+
+                    if(timeLeft == 0){
+                        timeCounter = 0;
+                        timer.cancel();
+                        timer = null;
+                        textViewResendCode.setTextColor(getResources().getColor(R.color.colorAccent));
+                        textViewResendCode.setText(getString(R.string.resend_code));
+                        textViewResendCode.setOnClickListener(onclicklistener);
+                    }
+                }
+            });
+        }
+    }
+
+    private void startTimer(int time){
+
+        if(timer != null){
+            timer.cancel();
+            timer = null;
+        }
+        timer = new Timer();
+        TimerTask = new timerTask();
+        timer.schedule(TimerTask, 5,time);
     }
 }
