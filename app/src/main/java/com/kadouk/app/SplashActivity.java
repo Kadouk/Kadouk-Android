607ebeac9070ae45444c71d5e381453ee5bfa2ee
mohamad_zoli <zolfaghari.mh94@gmail.com>
2018-12-11 02:35:11 +0330

    check conectivity to the internet in splash activity, check first run in splash activity

--- app/src/main/java/com/kadouk/app/SplashActivity.java
+++ app/src/main/java/com/kadouk/app/SplashActivity.java
@@ -2,14 +2,19 @@ package com.kadouk.app;
 
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.content.Intent;
 import android.util.Log;
+import android.widget.Toast;
 
 import com.kadouk.app.model.ContentRespons;
 import com.kadouk.app.webService.APIClient;
 import com.kadouk.app.webService.APIInterface;
+
+import java.net.InetAddress;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -26,22 +31,8 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        SharedPreferences = getSharedPreferences(MyShPref, Context.MODE_PRIVATE);
-        if (SharedPreferences.getString(FirstRun,null) == null) {
-            sendAPI();
+        checkConnection();
 
-            Log.i("token", "token1 = " + SharedPreferences.getString(FirstRun,null));
-            intent = new Intent(this, SignUpActivity.class);
-            finish();
-            startActivity(intent);
-        }else
-            startActivity(new Intent(SplashActivity.this, MainActivity.class));
-
-        if (SharedPreferences.getString(authenticationToken,null) != null) {
-            String Token = SharedPreferences.getString(authenticationToken,null);
-            Globals.setToken(Token);
-            Log.i("token", "token = " + SharedPreferences.getString(authenticationToken,null));
-        }
     }
 
 
@@ -74,4 +65,54 @@ public class SplashActivity extends AppCompatActivity {
         });
 
     }
+
+    protected boolean isOnline() {
+
+        ConnectivityManager cm = (ConnectivityManager)getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        NetworkInfo netInfo = cm.getActiveNetworkInfo();
+
+        if (netInfo != null && netInfo.isConnectedOrConnecting()) {
+
+            return true;
+
+        } else {
+
+            return false;
+
+        }
+
+    }
+
+    public void checkConnection(){
+
+        if(isOnline()){
+
+            SharedPreferences = getSharedPreferences(MyShPref, Context.MODE_PRIVATE);
+            if (SharedPreferences.getString(FirstRun,null) == null) {
+                sendAPI();
+
+                Log.i("token", "token1 = " + SharedPreferences.getString(FirstRun,null));
+                intent = new Intent(this, SignUpActivity.class);
+                finish();
+                startActivity(intent);
+            }else
+                startActivity(new Intent(SplashActivity.this, MainActivity.class));
+
+            if (SharedPreferences.getString(authenticationToken,null) != null) {
+                String Token = SharedPreferences.getString(authenticationToken,null);
+                Globals.setToken(Token);
+                Log.i("token", "token = " + SharedPreferences.getString(authenticationToken,null));
+            }
+
+        }else{
+
+            Toast.makeText(this, "net nist",
+                    Toast.LENGTH_LONG).show();
+
+        }
+
+    }
+
+
 }
